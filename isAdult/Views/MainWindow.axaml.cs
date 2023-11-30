8ddfac94d7adcebba5ea2d1826f6076efbb8d63d
Stepan <stepan.dubrov@gmqil.com>
2023-11-30 18:41:13 +0300

    datapicker

--- isAdult/Views/MainWindow.axaml.cs
+++ isAdult/Views/MainWindow.axaml.cs
@@ -1,4 +1,5 @@
 using Avalonia.Controls;
+using isAdult.ViewModels;
 
 namespace isAdult.Views;
 
@@ -7,5 +8,18 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    private void OnDataChange(object? sender, DatePickerSelectedValueChangedEventArgs e)
+    {
+        if (sender is DatePicker dtp && dtp.SelectedDate != null)
+        {
+            var selDate = dtp.SelectedDate.Value.LocalDateTime;
+            if (DataContext is MainWindowViewModel mVM)
+            {
+                mVM.Date = selDate;
+                mVM.IsAdult = mVM.isAdultFunc(selDate);
+            }
+        }
     }
 }
